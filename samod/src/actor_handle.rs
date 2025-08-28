42e292c13aca7cdcff93a7ee27e2075fc2e49f23
Alex Good <alex@memoryandthought.me>
2025-08-28 10:03:51 +0100

    Switch from futures::channel::mpsc and std::sync::mpsc::channel to async_channel
    Currently document actors run on a `rayon` thread pool and use a
    blocking `std::sync::mpsc::channel` to communicate with the main thread.
    I want to make the use of a thread pool optional, spawning the actors
    onto the runtime if there is no thread pool configured. This means that
    the communication channel between the main thread and the actors must be
    capable of async operations. `async_channel` is a stable, and widely
    used async channel implementation that works in both sync and async
    contexts. For consistency we use it everywhere, replacing also the uses
    of `futures::channel::mpsc`.

--- samod/src/actor_handle.rs
+++ samod/src/actor_handle.rs
@@ -5,6 +5,6 @@ use crate::{ActorTask, DocActorInner, DocHandle};
 pub(crate) struct ActorHandle {
     #[allow(dead_code)]
     pub(crate) inner: Arc<Mutex<DocActorInner>>,
-    pub(crate) tx: std::sync::mpsc::Sender<ActorTask>,
+    pub(crate) tx: async_channel::Sender<ActorTask>,
     pub(crate) doc: DocHandle,
 }
