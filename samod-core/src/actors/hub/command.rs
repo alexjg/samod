0fd762faae4519bfd638b137d081e0176efd92a5
Alex Good <alex@memoryandthought.me>
2025-09-11 12:08:53 +0100

    samod-core: add module docs

--- samod-core/src/actors/hub/command.rs
+++ samod-core/src/actors/hub/command.rs
@@ -3,6 +3,6 @@ mod command;
 mod command_id;
 mod command_result;
 
-pub use command::Command;
+pub(crate) use command::Command;
 pub use command_id::CommandId;
 pub use command_result::CommandResult;
