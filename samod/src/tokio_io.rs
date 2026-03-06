e2e4e8536159e21671a4fc32c20754ca3d412a8e
Lilith Silver <84940819+LilithSilver@users.noreply.github.com>
2026-03-06 13:45:54 -0800

    simplify tcp dialer

--- samod/src/tokio_io.rs
+++ samod/src/tokio_io.rs
@@ -1,14 +1,16 @@
-use std::net::SocketAddr;
+use std::sync::Arc;
 
 use futures::FutureExt;
+use url::Url;
 
-use crate::{Dialer, Transport};
+use crate::{Dialer, Repo, Transport};
 
 /// A [`Dialer`] that connects to a TCP endpoint
 ///
-/// The dialer can be constructed from either a `SocketAddr` or a host/port
-/// pair. In the latter case, the host will be resolved to an IP address using
-/// [`tokio::net::lookup_host`].
+/// The dialer is constructed via a [Url]. The host will be resolved using
+/// [`tokio::net::TcpStream::connect`] on [`Url::host_str()`] and [`Url::port()`]. This is the same
+/// as calling [`tokio::net::lookup_host`] directly.
+/// Any more advanced resolution should be performed with a custom [`Dialer`] implementation.
 ///
 /// The dialer will use a simple length delimited framing to connect to the
 /// other end. Accepting connections on the other end should be done using
@@ -26,7 +28,6 @@ use crate::{Dialer, Transport};
 ///
 /// // First start a server
 /// let listener = TcpListener::bind("0.0.0.0:0").await.unwrap();
-/// let port = listener.local_addr().unwrap().port();
 /// tokio::spawn(async move {
 ///     let repo: Repo = Repo::build_tokio().load().await;
 ///     let acceptor = repo.make_acceptor(url::Url::parse("tcp://someserver").unwrap()).unwrap();
@@ -36,47 +37,24 @@ use crate::{Dialer, Transport};
 ///
 /// // Now make a client which dials the server
 /// let repo: Repo = Repo::build_tokio().load().await;
-/// let dialer = TcpDialer::new_host_port("0.0.0.0", port);
+/// let dialer = TcpDialer::new(url::Url::parse("tcp://0.0.0.0:0").unwrap());
 /// repo.dial(BackoffConfig::default(), Arc::new(dialer)).unwrap();
 /// # }
 /// ```
 pub struct TcpDialer {
-    host: Host,
+    host: Url,
 }
 
 impl TcpDialer {
-    /// Create a dialer which will attempt to connect to the given socket address
-    pub fn new_socket_addr(addr: SocketAddr) -> Self {
-        Self {
-            host: Host::SocketAddr(addr),
-        }
+    /// Create a dialer which will resolve the given [Url].
+    pub fn new(url: Url) -> Self {
+        Self { host: url }
     }
-
-    /// Create a dialer which will first attempt to resolve the given host, and
-    /// then connect to the resolved address and port.
-    pub fn new_host_port<S: AsRef<str>>(host: S, port: u16) -> Self {
-        Self {
-            host: Host::HostPort(host.as_ref().to_string(), port),
-        }
-    }
-}
-
-#[derive(Clone)]
-enum Host {
-    SocketAddr(SocketAddr),
-    HostPort(String, u16),
 }
 
 impl Dialer for TcpDialer {
     fn url(&self) -> url::Url {
-        match self.host {
-            Host::SocketAddr(addr) => {
-                url::Url::parse(&format!("tcp://{}:{}", addr.ip(), addr.port())).unwrap()
-            }
-            Host::HostPort(ref host, port) => {
-                url::Url::parse(&format!("tcp://{}:{}", host, port)).unwrap()
-            }
-        }
+        self.host.clone()
     }
 
     fn connect(
@@ -85,32 +63,67 @@ impl Dialer for TcpDialer {
         'static,
         Result<crate::Transport, Box<dyn std::error::Error + Send + Sync + 'static>>,
     > {
-        let host = self.host.clone();
+        let url = self.host.clone();
         async move {
-            let addr = match host {
-                Host::SocketAddr(addr) => addr,
-                Host::HostPort(ref host, port) => {
-                    tracing::trace!("resolving {}:{}", host, port);
-                    let mut addrs = tokio::net::lookup_host((host.as_str(), port))
-                        .await?
-                        .filter(|a| a.is_ipv4());
-                    let Some(addr) = addrs.next() else {
-                        return Err::<_, Box<dyn std::error::Error + Send + Sync + 'static>>(
-                            Box::new(std::io::Error::new(
-                                std::io::ErrorKind::NotFound,
-                                format!("no addresses found for {}:{}", host, port),
-                            )),
-                        );
-                    };
-                    tracing::trace!("resolved {}:{} to {}", host, port, addr);
-                    addr
-                }
+            if url.scheme() != "tcp" {
+                return Err::<_, Box<dyn std::error::Error + Send + Sync + 'static>>(Box::new(
+                    std::io::Error::new(
+                        std::io::ErrorKind::InvalidInput,
+                        format!("Provided URL {url} is not of scheme tcp://!"),
+                    ),
+                ));
             };
-            tracing::debug!("dialing {}:{}", addr.ip(), addr.port());
-            let io = tokio::net::TcpStream::connect(addr).await?;
+
+            let Some(host) = url.host_str() else {
+                return Err::<_, Box<dyn std::error::Error + Send + Sync + 'static>>(Box::new(
+                    std::io::Error::new(
+                        std::io::ErrorKind::InvalidInput,
+                        format!("No host provided for {url}!"),
+                    ),
+                ));
+            };
+
+            let Some(port) = url.port() else {
+                return Err::<_, Box<dyn std::error::Error + Send + Sync + 'static>>(Box::new(
+                    std::io::Error::new(
+                        std::io::ErrorKind::InvalidInput,
+                        format!("No port provided for {url}!"),
+                    ),
+                ));
+            };
+
+            let io = tokio::net::TcpStream::connect((host, port)).await?;
             let transport = Transport::from_tokio_io(io);
             Ok(transport)
         }
         .boxed()
+    }
+}
+
+impl Repo {
+    /// Dial a TCP endpoint with automatic reconnection.
+    ///
+    /// Uses a built-in TCP [`Dialer`](crate::Dialer) that connects
+    /// via `tokio-io`. The connection will automatically reconnect
+    /// with the provided backoff configuration when the connection is lost.
+    ///
+    /// This is a convenience wrapper around [`Repo::dial`] with a
+    /// [`TcpDialer`].
+    ///
+    /// # Arguments
+    ///
+    /// * `url` - The TCP URL to connect to (e.g. `"tcp://sync.example.com"`).
+    /// * `backoff` - Backoff configuration for reconnection attempts.
+    ///
+    /// # Returns
+    ///
+    /// A [`DialerHandle`](crate::DialerHandle) for observing and controlling the dialer.
+    pub fn dial_tcp(
+        &self,
+        url: Url,
+        backoff: crate::BackoffConfig,
+    ) -> Result<crate::DialerHandle, crate::Stopped> {
+        let dialer = Arc::new(TcpDialer::new(url));
+        self.dial(backoff, dialer)
     }
 }
